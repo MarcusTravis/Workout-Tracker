e3f79c3c434b582ef7b19ab6db114c098bad41d0
Marcus <mpt789@gmail.com>
2020-06-26 14:24:15 -0700

    built api routes and schema inside models folder

--- server.js
+++ server.js
@@ -1,30 +1,72 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
 const app = express();
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
-
-app.post("/submit", ({body}, res) => {
-  User.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
 });
+
+// require("./routes/apiroutes")(app);
+// require("./routes/htmlroutes")(app);
+module.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  app.get("/api/workouts/range", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  app.post("/api/workouts/range", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  app.post("/api/workouts", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => {
+        console.log("err", err);
+        res.json(err);
+      });
+  });
+};
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
